9c0b239785ace1dbdd5b69913e5d9ab107fd45e5
Subodh7300 <97154559+Subodh7300@users.noreply.github.com>
2023-07-15 11:50:30 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+from typing import List 
+
+
+HYPHEN_E_DOT = '-e .'
+
+def get_packages(file_path: str) -> List[str]:
+    '''
+    This function will return the list of requirement packages
+    '''
+    requirements = []
+    with open(file_path) as file:
+        requirements = file.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+
+    if HYPHEN_E_DOT in requirements:
+        requirements.remove(HYPHEN_E_DOT)
+        
+    return requirements
+
+
+setup(
+    name='one-piece-qna',
+    version='0.0.1',
+    author='Subodh',
+    author_email='s.subodh7976@gmail.com',
+    packages=find_packages(),
+    install_requires=get_packages('requirements.txt')
+)
